2a9577a0513eeb28751aa452c465d66c300cfcc4
Thinh Nguyen <thinh.nguyen@codeleap.de>
2024-07-13 21:51:36 +0700

    feat: Init file version.tf

---
+++ infrastructure/terraform/version.tf
@@ -0,0 +1,14 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.108.0"
+    }
+  }
+  backend "azurerm" {
+  }
+}
